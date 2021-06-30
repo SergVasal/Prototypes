fb1e1f73b78b3e490f8dc35fe6f2cd827aab85b2
SergVasal <sergvasal90@gmail.com>
2021-06-30 20:55:26 +0300

    Array

---
+++ DataStructures/Assets/Scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    void Start()
+    {
+        int[,,] array3d = new int[2,3,4];
+
+        var allLength = array3d.Length;
+        Debug.Log($"allLength: {allLength}");
+        var total = 1;
+        for (int i = 0; i < array3d.Rank; i++)
+        {
+            total *= array3d.GetLength(i);
+            Debug.Log($"Element count in rank {i}: {array3d.GetLength(i)} Current total: {total}");
+        }
+
+    }
+}
