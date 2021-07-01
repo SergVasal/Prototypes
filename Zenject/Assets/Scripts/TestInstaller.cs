f91c5f4a5d139ab01ec7928777f7dcaa270352aa
SergVasal <sergvasal90@gmail.com>
2021-07-01 15:48:47 +0300

    Add test intstaller

---
+++ Zenject/Assets/Scripts/TestInstaller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class TestInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<Greeter>().AsSingle().NonLazy();
+        Container.Bind<string>().FromInstance("Hello World!");
+    }
+}
+
+public class Greeter
+{
+    public Greeter(string message)
+    {
+        Debug.Log(message);
+    }
+}
