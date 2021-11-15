6546165dfac7bb012756161c74a6d73e0033c22b
SergVasal <sergvasal90@gmail.com>
2021-11-15 18:49:30 +0300

    Start MathTank project

---
+++ MathTank/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform player;
+
+        private void Update()
+        {
+            var playerPosition = player.transform.position;
+            var cameraTransform = transform;
+            cameraTransform.position = new Vector3(playerPosition.x, playerPosition.y, cameraTransform.position.z);
+        }
+    }
+}
---
+++ MathTank/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace DefaultNamespace
+{
+    public class ObjectManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject objectPrefab;
+
+        private void Start()
+        {
+            var obj = Instantiate(objectPrefab, new Vector3(Random.Range(-100, 100), Random.Range(-100, 100), objectPrefab.transform.position.z), Quaternion.identity);
+            Debug.Log($"Fuel location: {obj.transform.position}");
+        }
+    }
+}
